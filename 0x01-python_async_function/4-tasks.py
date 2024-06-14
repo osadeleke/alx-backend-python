5c08f20469c1b9ed9a9c1de2d558ea0aad21f85d
Olusegun Adeleke <oadeleke123@gmail.com>
2024-06-14 21:01:31 +0100

    Add: task wait randomy using asyncio tasks

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+altering a function into a new function
+"""
+
+from typing import List
+
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    method to wait for return value
+    """
+    wait_list: list = []
+    for n in range(n):
+        task = task_wait_random(max_delay)
+        wait_list.append(await task)
+    wait_list.sort()
+    return wait_list
