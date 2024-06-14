d7a5a8c9453eaa6aa691ae38c8c3efab40e5d6a1
Olusegun Adeleke <oadeleke123@gmail.com>
2024-06-14 18:52:39 +0100

    fix: list sorting, documentation and indentation

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -12,8 +12,12 @@ from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
-  wait_list: list = []
-  for n in range(n):
-    wait_list.append(await wait_random(max_delay))
-  return wait_list
+    """
+    method to wait for return value
+    """
+    wait_list: list = []
+    for n in range(n):
+        wait_list.append(await wait_random(max_delay))
+    return sorted(wait_list)
