7fefc83dacb1b62c50642ac3afb5c3473a7a3ef9
Olusegun Adeleke <oadeleke123@gmail.com>
2024-06-14 19:35:35 +0100

    Add: create asycio class

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Tasks module
+"""
+from typing import Awaitable
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> Awaitable:
+    """
+    return:
+       asyncio task
+    """
+    return asyncio.create_task(wait_random(max_delay))
